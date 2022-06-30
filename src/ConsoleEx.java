8da2840c0915266b6982ae4c8fb9ff7b006fa788
M1ttwsuf <ttwsuf@naver.com>
2022-06-30 12:12:09 +0900

    init

---
+++ src/ConsoleEx.java
@@ -0,0 +1,18 @@
+
+
+public class ConsoleEx {
+    public static void main(String[] args){
+        Console console = System.console();
+
+        System.out.print("아이디: ");
+        String id = console.readLine();
+
+        System.out.print("패스워드: ");
+        char[]  charPass = console.readPassword();
+        String strPassword = new String(charPass);
+
+        System.out.println("------------------------");
+        System.out.println(id);
+        System.out.println(strPassword);
+    }
+}
